ec71c49cdb40096ef95ce48b4df4e4cdc49f6930
Melissa Sweeney <msc18@cornell.edu>
2025-12-19 15:35:48 -0500

    FCS Header, Nav and Footer

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -2,11 +2,37 @@
     <div class="band main-footer padded">
         <div class="container">
             <div class="layout layout-1-2">
-                <div class="section">
-                    <h2 class="h4">Footer Primary</h2>
+                <div class="section primary-footer">
+                    <h2 class="h2">FCS Customer Service Center</h2>
+                    <p>
+                        <a href="https://findfacilities-prod.fcs.cornell.edu/facilitypage/2023"> Humphreys Service Building</a><br>
+                        Cornell University<br>
+                        Ithaca, NY 14853
+                    </p>
+                    <p>
+                        <a class="link-phone" href="tel:+1-607-255-5322">(607) 255-5322</a><br>
+                    </p>
                 </div>
-                <div class="sectiont">
-                    <h2 class="h4">Footer Secondary</h2>
+                <div class="section secondary-footer">
+                    <div>
+                        <h2 class="h2">See Also</h2>
+                        <ul class="list-menu vertical">
+                            <li><a href="{{config('app.fcssite')}}/contact-fcs">Contact FCS</a></li>
+                            <li><a href="{{config('app.fcssite')}}/about-us/fcs-org-chart">Org Chart</a></li>
+                            <li><a href="{{config('app.fcssite')}}/about-us/leadership">FCS Leadership</a></li>
+                            <li><a href="https://hr.cornell.edu/jobs">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 class="h2">Cornell Quick Links</h2>
+                        <ul class="list-menu vertical">
+                            <li><a href="http://www.cornell.edu/">Cornell Home Page</a></li>
+                            <li><a href="http://cuinfo.cornell.edu/">CUInfo</a></li>
+                            <li><a href="http://www.cornell.edu/cuinfo/specialconditions/">Special Conditions</a></li>
+                            <li><a href="http://www.cornell.edu/about/maps/">Campus Maps</a></li>
+                            <li><a href="https://shibidp.cit.cornell.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=http://www.workday.com">Workday</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/components/layouts/main-nav.blade.php
+++ resources/views/components/layouts/main-nav.blade.php
@@ -1,9 +1,32 @@
-<nav id="main-navigation" class="navbar navbar-tall fill dark active-red dropdown-menu dropdown-menu-on-demand" aria-label="Main">
+<nav id="main-navigation" class="navbar nav-left fill dark dropdown-menu dropdown-menu-on-demand" aria-label="Main">
     <div class="container" aria-label="Dropdown Menus">
         <a id="mobile-home" href="#"><span class="sr-only">Home</span></a>
         <button id="mobile-close"><span class="sr-only">Close menu</span></button>
         <ul>
             <li><a href="/">Home</a></li>
+        </ul>
+        {{-- Testing only  --}}
+        <ul>
+            <li>
+                <a role="combobox" aria-expanded="false" data-aria-controls="dropdown-examples" aria-controls="dropdown-examples" tabindex="0">Examples</a>
+                <ul id="dropdown-examples" role="listbox">
+                    <li>
+                        <a href="">Cornell Design System</a>
+                    </li>
+                    <li>
+                        <a href="">Forms</a>
+                    </li>
+                    <li>
+                        <a href="">Errors</a>
+                    </li>
+                    <li>
+                        <a href="">Table</a>
+                    </li>
+                    <li>
+                        <a href="">User Examples</a>
+                    </li>
+                </ul>
+            </li>
         </ul>
 
     </div>
